f5207b136246de0f40471339849d8e178462055f
kotalavaishnavi <94115776+kotalavaishnavi@users.noreply.github.com>
2021-11-24 01:05:59 +0530

    main

--- 3_Implementation/test/test_librarymanagement.c
+++ 3_Implementation/test/test_librarymanagement.c
@@ -1,1 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
 
+#include <LibraryManagement.h>
+
+/* Modify these two lines according to the project */
+#include <LibraryManagement.h>
+#define PROJECT_NAME    "LibraryManagement"
